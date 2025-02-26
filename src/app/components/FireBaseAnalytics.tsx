a624b7f08f68c951cb6ac396eaabb19e9186ce75
Anand Patil <anand.patil.ap79@gmail.com>
2025-02-26 20:49:47 +0100

    update analytics code

---
+++ src/app/components/FireBaseAnalytics.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useEffect } from "react";
+import { initializeApp } from "firebase/app";
+import { getAnalytics, logEvent } from "firebase/analytics";
+
+const FirebaseAnalytics = () => {
+  useEffect(() => {
+    const firebaseConfig = {
+      apiKey: "AIzaSyDT8ynIROJl3AnZ_TQii-wN48QGkEcgGdc",
+      authDomain: "contact-form-portfolio-20552.firebaseapp.com",
+      projectId: "contact-form-portfolio-20552",
+      storageBucket: "contact-form-portfolio-20552.firebasestorage.app",
+      messagingSenderId: "768901717572",
+      appId: "1:768901717572:web:73bc35d1e00e4077e3e611",
+      measurementId: "G-8HLJYE7JDT",
+    };
+
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+
+    // Log a custom event (optional)
+    logEvent(analytics, "page_view", {
+      page_path: window.location.pathname,
+    });
+  }, []);
+
+  return null; // This component doesn't render anything visually
+};
+
+export default FirebaseAnalytics;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata, Viewport } from "next";
 import { Poppins, Roboto } from "next/font/google";
 import "./globals.css";
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+
+import FirebaseAnalytics from "./components/FireBaseAnalytics";
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700", "800", "900"],
@@ -33,23 +33,12 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const firebaseConfig = {
-    apiKey: "AIzaSyDT8ynIROJl3AnZ_TQii-wN48QGkEcgGdc",
-    authDomain: "contact-form-portfolio-20552.firebaseapp.com",
-    projectId: "contact-form-portfolio-20552",
-    storageBucket: "contact-form-portfolio-20552.firebasestorage.app",
-    messagingSenderId: "768901717572",
-    appId: "1:768901717572:web:73bc35d1e00e4077e3e611",
-    measurementId: "G-8HLJYE7JDT",
-  };
-  const app = initializeApp(firebaseConfig);
-
-  const analytics =
-    app.name && typeof window !== "undefined" ? getAnalytics(app) : null;
-
   return (
     <html lang="en">
-      <body className={`${poppins.className} antialiased`}>{children}</body>
+      <body className={`${poppins.className} antialiased`}>
+        {children}
+        <FirebaseAnalytics />
+      </body>
     </html>
   );
 }
