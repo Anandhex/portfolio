d28dcbed802c0bde935b3df0dacf623dd31b7921
Anand Patil <anand.patil.ap79@gmail.com>
2025-02-26 20:27:34 +0100

    added firebase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata, Viewport } from "next";
 import { Poppins, Roboto } from "next/font/google";
 import "./globals.css";
-
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700", "800", "900"],
@@ -32,19 +33,6 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={`${poppins.className} antialiased`}>{children}
-      
-      <script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
     apiKey: "AIzaSyDT8ynIROJl3AnZ_TQii-wN48QGkEcgGdc",
     authDomain: "contact-form-portfolio-20552.firebaseapp.com",
@@ -52,15 +40,13 @@ export default function RootLayout({
     storageBucket: "contact-form-portfolio-20552.firebasestorage.app",
     messagingSenderId: "768901717572",
     appId: "1:768901717572:web:73bc35d1e00e4077e3e611",
-    measurementId: "G-8HLJYE7JDT"
+    measurementId: "G-8HLJYE7JDT",
   };
-
-  // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
-</script>
-      
-      </body>
+  return (
+    <html lang="en">
+      <body className={`${poppins.className} antialiased`}>{children}</body>
     </html>
   );
 }
