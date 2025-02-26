89ffa638476e5b04f0678e1cb73cfbdf9f438a0e
Anand Patil <anand.patil.ap79@gmail.com>
2025-02-26 21:21:08 +0100

    new changes

--- src/app/components/FireBaseAnalytics.tsx
+++ src/app/components/FireBaseAnalytics.tsx
@@ -8,34 +8,36 @@ const FirebaseAnalytics = () => {
   useEffect(() => {
     if (typeof window !== "undefined") {
       // Check if Firebase is already initialized, and only initialize if not
-      if (!getApps().length) {
-        const firebaseConfig = {
-          apiKey: "AIzaSyDT8ynIROJl3AnZ_TQii-wN48QGkEcgGdc",
-          authDomain: "contact-form-portfolio-20552.firebaseapp.com",
-          projectId: "contact-form-portfolio-20552", // Ensure projectId is here
-          storageBucket: "contact-form-portfolio-20552.firebasestorage.app",
-          messagingSenderId: "768901717572",
-          appId: "1:768901717572:web:73bc35d1e00e4077e3e611",
-          measurementId: "G-8HLJYE7JDT",
-        };
+      if (typeof window !== "undefined") {
+        console.log("Running on client-side");
+        console.log("Initialized Firebase Apps:", getApps());
 
-        // Only initialize Firebase if it hasn't been initialized yet
-        const app = initializeApp(firebaseConfig);
-        const analytics = getAnalytics(app);
+        try {
+          // Check if a Firebase app already exists
+          const app = getApp(); // get the default app, should already exist if initialized
+          const analytics = getAnalytics(app);
+          logEvent(analytics, "page_view", {
+            page_path: window.location.pathname,
+          });
+        } catch (error) {
+          // If no Firebase app exists, initialize it
+          console.log("Initializing Firebase...");
+          const firebaseConfig = {
+            apiKey: "AIzaSyDT8ynIROJl3AnZ_TQii-wN48QGkEcgGdc",
+            authDomain: "contact-form-portfolio-20552.firebaseapp.com",
+            projectId: "contact-form-portfolio-20552",
+            storageBucket: "contact-form-portfolio-20552.firebasestorage.app",
+            messagingSenderId: "768901717572",
+            appId: "1:768901717572:web:73bc35d1e00e4077e3e611",
+            measurementId: "G-8HLJYE7JDT",
+          };
 
-        // Log a custom event (optional)
-        logEvent(analytics, "page_view", {
-          page_path: window.location.pathname,
-        });
-      } else {
-        // If Firebase is already initialized, get the existing app instance
-        const app = getApp();
-        const analytics = getAnalytics(app);
-
-        // Log a custom event (optional)
-        logEvent(analytics, "page_view", {
-          page_path: window.location.pathname,
-        });
+          const app = initializeApp(firebaseConfig);
+          const analytics = getAnalytics(app);
+          logEvent(analytics, "page_view", {
+            page_path: window.location.pathname,
+          });
+        }
       }
     }
   }, []); // Only run this effect once on mount
